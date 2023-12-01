1635cea8a1116e3f3cabbc552af6a22053c73f16
Eli <elimbukoski@gmail.com>
2023-12-01 13:25:20 -0500

    add poi classes

--- Common.py
+++ Common.py
@@ -4,6 +4,7 @@ import math
 class Position:
     def __init__(self, a=0, b=0, isCartesian=True):
         """Position class for storing x,y coordinates in either cartesian or polar coordinates.
+            Not applicable as a POI.
 
         Args:
             a (int, optional): Either x or r. Defaults to 0.
@@ -68,9 +69,72 @@ class Position:
         return math.sqrt(self.x**2 + self.y**2)
 
 
+class POIPoint:
+    def __init__(self, Point=Position(), color="red"):
+        """Point class for drawing points. Applicable as a POI.
+
+        Args:
+            Point (Position, optional): Point to draw. Defaults to Position().
+            color (str, optional): Color of the point. Defaults to "red".
+        """
+        self.Point = Point
+        self.color = color
+
+    def Canvas(self, canvas, SideSize, GuiScale):
+        """Draws the position on a tkinter canvas.
+
+        Args:
+            canvas (tkinter.Canvas): Canvas to draw the position on.
+            SideSize (int): Size of the canvas.
+            GuiScale (int): Scale of the canvas.
+        """
+        canvas.oval(
+            (self.Point.x - 0.1 + SideSize / 2) * GuiScale,
+            (self.Point.y - 0.1 + SideSize / 2) * GuiScale,
+            (self.Point.x + 0.1 + SideSize / 2) * GuiScale,
+            (self.Point.y + 0.1 + SideSize / 2) * GuiScale,
+            color=self.color,
+        )
+
+
+class Line:
+    def __init__(self, Point1=Position(), Point2=Position(), color="green"):
+        """Line class for drawing lines. Applicable as a POI.
+
+        Args:
+            Point1 (Position, optional): First point of the line. Defaults to Position().
+            Point2 (Position, optional): Second point of the line. Defaults to Position().
+        """
+
+        self.Point1 = Point1
+        self.Point2 = Point2
+        self.color = color
+
+    def __str__(self):
+        return f"Line from {self.Point1} to {self.Point2}"
+
+    def Canvas(self, canvas, SideSize, GuiScale):
+        """Draws the line on a tkinter canvas.
+
+        Args:
+            canvas (tkinter.Canvas): Canvas to draw the line on.
+            SideSize (int): Size of the canvas.
+            GuiScale (int): Scale of the canvas.
+        """
+        canvas.line(
+            (self.Point1.x + SideSize / 2) * GuiScale,
+            (self.Point1.y + SideSize / 2) * GuiScale,
+            (self.Point2.x + SideSize / 2) * GuiScale,
+            (self.Point2.y + SideSize / 2) * GuiScale,
+            width=2,
+            color=self.color,
+        )
+
+
 class Rock:
     def __init__(self, pos=Position(0, 0), diameter=0.5):
         """A rock object that stores its position and diameter.
+            Not Applicable as a POI.
 
         Args:
             pos (Position, optional): Location of the rock. Defaults to Position(0,0).
